d0eb582bfe530a70080496b60c26fa306e1c796c
Vaishali Rawat <rawatvaishali254@gmail.com>
2025-05-03 21:41:52 +0530

    express project : Set up basic Express server with contact routes and middleware

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+const port  = process.env.PORT || 4000;
+
+app.use("/api/contacts", require("./routes/contactRoute"));
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
