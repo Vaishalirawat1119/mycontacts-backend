aebbee17ddc0d089172f5a72c851c6f2965b4bea
Vaishali Rawat <rawatvaishali254@gmail.com>
2025-05-06 15:08:09 +0530

    feat: add user routes to the application under /api/users

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port  = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoute"));
+app.use("/api/users", require("./routes/userRoute"));
+
 app.use(errorHandler);
 
 app.listen(port, () => {
